2e9bc3a2c2b92ab9e5f694f47de26dd388f48d54
jokkerking-bots <63076000+jokkerking-bots@users.noreply.github.com>
2020-04-07 11:51:28 +0500

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -42,12 +42,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n💽Yuklanmoqda: {2}%\n".format(
+        progress = "[{0}{1}] \n💽Progress: {2}%\n".format(
             ''.join(["■" for i in range(math.floor(percentage / 5))]),
             ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n📥Yuklash tezligi: {2}/s\n⌚Qolgan Taxminiy Vaqt: {3}\n".format(
+        tmp = progress + "{0} of {1}\n📥Speed: {2}/s\n⌚Remaining: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
